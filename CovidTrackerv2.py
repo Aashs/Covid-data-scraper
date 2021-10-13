48c39e5f8c8c0ba2494aa444704b898cabc48d0f
v1s1t0r999 <aditya.funs.11@gmail.com>
2021-10-13 17:11:29 +0530

    Create CovidTrackerv2.py

---
+++ CovidTrackerv2.py
@@ -0,0 +1,38 @@
+import reqeusts as r
+
+
+class CountryNotFound(s):
+	pass
+
+class CovidClient:
+	def __init__(self):
+		self.country=""
+		
+	def _data(self):
+		"""Internal Function to load data"""
+		s = r.get(f"https://corona.lmao.ninja/v3/covid-19/countries/{self.country}")
+		try:
+			s.json()['message']
+			return False
+		except KeyError:
+			return s.json()
+		
+	def load_country(self,country:str):
+		self.country=country
+		if self._data() is False:
+			raise CountryNotFound("No such Country Found!!")
+			return False
+		else:
+			return True
+	
+	
+	def cases(self):
+		data = self._data()
+		active = data['active']
+		critical = data['critical']
+		total = data['cases']
+		return {'total':total,'active':active,'critical':critical}
+		
+		
+		
+		
