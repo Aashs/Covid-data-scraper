13ed60269ff81a33fa69180018a3e58c34d50fbf
Aasher <aasher.aashes@gmail.com>
2021-10-28 18:56:38 +0530

    Finished the project just have to do saving to txt and all

--- covid.py
+++ covid.py
@@ -3,35 +3,33 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
-"""Getting cases"""
-
-
-s = time.time()
-link = "https://www.worldometers.info/coronavirus"
-req = requests.get(link)
-if req.status_code == 200:
-    s = time.time()
-    str_content = bytes.decode(req.content)
-    ind = str_content.lower().find('<div class="maincounter-number"')
-    cases = str_content[ind:ind + 100]
-    cases = BeautifulSoup(cases, "lxml")
-    str_content = str_content[ind + 100:]
-    ind = str_content.lower().find('<div class="maincounter-number"')
-    deaths = str_content[ind:ind + 100]
-    deaths = BeautifulSoup(deaths, "lxml")
-    str_content = str_content[ind + 100:]
-    ind = str_content.lower().find('<div class="maincounter-number"')
-    recover = str_content[ind:ind + 100]
-    recover = BeautifulSoup(recover, "lxml")
-    cases = cases.span.string
-    deaths = deaths.span.string
-    recover = recover.span.string
-
 """Total Cases"""
+
+print('test')
 
 
 def total_cases():
-    print(f"cases: {cases}\ndeaths: {deaths}\nrecover: {recover}")
+    s = time.time()
+    link = "https://www.worldometers.info/coronavirus"
+    req = requests.get(link)
+    if req.status_code == 200:
+        s = time.time()
+        str_content = bytes.decode(req.content)
+        ind = str_content.lower().find('<div class="maincounter-number"')
+        cases = str_content[ind:ind + 100]
+        cases = BeautifulSoup(cases, "lxml")
+        str_content = str_content[ind + 100:]
+        ind = str_content.lower().find('<div class="maincounter-number"')
+        deaths = str_content[ind:ind + 100]
+        deaths = BeautifulSoup(deaths, "lxml")
+        str_content = str_content[ind + 100:]
+        ind = str_content.lower().find('<div class="maincounter-number"')
+        recover = str_content[ind:ind + 100]
+        recover = BeautifulSoup(recover, "lxml")
+        cases = cases.span.string
+        deaths = deaths.span.string
+        recover = recover.span.string
+        print(f"cases: {cases}\ndeaths: {deaths} \nrecover: {recover}")
 
 
 """Cases by searching country"""
@@ -39,13 +37,30 @@ def total_cases():
 
 def country_search(country: str):
     try:
-        link = f"https://www.worldometers.info/coronavirus/country/{country}/"
-        print(f"cases: {cases}\ndeaths: {deaths}\nrecover: {recover}")
+        s = time.time()
+        link = f"https://www.worldometers.info/coronavirus/country/{country}"
+        req = requests.get(link)
+        if req.status_code == 200:
+            s = time.time()
+            str_content = bytes.decode(req.content)
+            ind = str_content.lower().find('<div class="maincounter-number"')
+            cases = str_content[ind:ind + 100]
+            cases = BeautifulSoup(cases, "lxml")
+            str_content = str_content[ind + 100:]
+            ind = str_content.lower().find('<div class="maincounter-number"')
+            deaths = str_content[ind:ind + 100]
+            deaths = BeautifulSoup(deaths, "lxml")
+            str_content = str_content[ind + 100:]
+            ind = str_content.lower().find('<div class="maincounter-number"')
+            recover = str_content[ind:ind + 100]
+            recover = BeautifulSoup(recover, "lxml")
+            cases = cases.span.string
+            deaths = deaths.span.string
+            recover = recover.span.string
+            print(f"cases: {cases}\ndeaths: {deaths}\nrecover: {recover}")
 
     except KeyError:
-        raise ValueError(
-            f"There is no country called '{country}'"
-        )
+        raise ValueError(f"There is no country called {country}")
 
 
 country_search('poland')
