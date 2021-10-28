5269dfab7cc72249a24b5e1693ad59f708c96cd9
Aasher <aasher.aashes@gmail.com>
2021-10-28 12:05:40 +0530

    Added some functions to get covid info

--- covid.py
+++ covid.py
@@ -3,31 +3,35 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
+"""Getting cases"""
+
+
+s = time.time()
+link = "https://www.worldometers.info/coronavirus"
+req = requests.get(link)
+if req.status_code == 200:
+    s = time.time()
+    str_content = bytes.decode(req.content)
+    ind = str_content.lower().find('<div class="maincounter-number"')
+    cases = str_content[ind:ind + 100]
+    cases = BeautifulSoup(cases, "lxml")
+    str_content = str_content[ind + 100:]
+    ind = str_content.lower().find('<div class="maincounter-number"')
+    deaths = str_content[ind:ind + 100]
+    deaths = BeautifulSoup(deaths, "lxml")
+    str_content = str_content[ind + 100:]
+    ind = str_content.lower().find('<div class="maincounter-number"')
+    recover = str_content[ind:ind + 100]
+    recover = BeautifulSoup(recover, "lxml")
+    cases = cases.span.string
+    deaths = deaths.span.string
+    recover = recover.span.string
+
 """Total Cases"""
 
 
 def total_cases():
-    s = time.time()
-    req = requests.get("https://www.worldometers.info/coronavirus/")
-    if req.status_code == 200:
-        s = time.time()
-        str_content = bytes.decode(req.content)
-        ind = str_content.lower().find('<div class="maincounter-number"')
-        cases = str_content[ind:ind + 100]
-        cases = BeautifulSoup(cases, "lxml")
-        str_content = str_content[ind + 100:]
-        ind = str_content.lower().find('<div class="maincounter-number"')
-        deaths = str_content[ind:ind + 100]
-        deaths = BeautifulSoup(deaths, "lxml")
-        str_content = str_content[ind + 100:]
-        ind = str_content.lower().find('<div class="maincounter-number"')
-        recover = str_content[ind:ind + 100]
-        recover = BeautifulSoup(recover, "lxml")
-        cases = cases.span.string
-        deaths = deaths.span.string
-        recover = recover.span.string
-
-        print(f"cases: {cases}\ndeaths: {deaths}\nrecover: {recover}")
+    print(f"cases: {cases}\ndeaths: {deaths}\nrecover: {recover}")
 
 
 """Cases by searching country"""
@@ -35,31 +39,13 @@ def total_cases():
 
 def country_search(country: str):
     try:
-        req = requests.get(
-            f"https://www.worldometers.info/coronavirus/country/{country}/")
-        s = time.time()
-        if req.status_code == 200:
-            s = time.time()
-            str_content = bytes.decode(req.content)
-            ind = str_content.lower().find('<div class="maincounter-number"')
-            cases = str_content[ind:ind + 100]
-            cases = BeautifulSoup(cases, "lxml")
-            str_content = str_content[ind + 100:]
-            ind = str_content.lower().find('<div class="maincounter-number"')
-            deaths = str_content[ind:ind + 100]
-            deaths = BeautifulSoup(deaths, "lxml")
-            str_content = str_content[ind + 100:]
-            ind = str_content.lower().find('<div class="maincounter-number"')
-            recover = str_content[ind:ind + 100]
-            recover = BeautifulSoup(recover, "lxml")
-
-            cases = cases.span.string
-            deaths = deaths.span.string
-            recover = recover.span.string
-
-            print(f"cases: {cases}\ndeaths: {deaths}\nrecover: {recover}")
+        link = f"https://www.worldometers.info/coronavirus/country/{country}/"
+        print(f"cases: {cases}\ndeaths: {deaths}\nrecover: {recover}")
 
     except KeyError:
         raise ValueError(
             f"There is no country called '{country}'"
         )
+
+
+country_search('poland')
