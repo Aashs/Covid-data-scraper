011a2e7bd173c473b9799904c514d7782e401bbb
Aasher <aasher.aashes@gmail.com>
2021-12-23 22:05:13 +0530

    API in work

--- covid.py
+++ covid.py
@@ -3,9 +3,6 @@ import time
 import requests
 import json
 
-with open('config.json') as f:
-    data = json.load(f)
-
 """Getting data on what to scrape"""
 
 
@@ -49,20 +46,6 @@ def all_countrys():
 '''Loads cases in json'''
 
 
-def test():
-    with open("cases_storage.json") as f:
-        data = json.load(f)
-        for elements in data["countries"].values():
-            del elements['increase-today']
-            print(elements)
-
-    with open('cases_storage.json', "w") as f:
-        json.dump(data, f, indent=3)
-
-
-test()
-
-
 def all_countrys_cases():
     while True:
         with open("cases_storage.json") as f:
@@ -101,8 +84,7 @@ def all_countrys_cases():
 
             except AttributeError:
                 print(f"No span found for {countries_req}")
+        time.sleep(1)
 
-        time.sleep(3600)
 
-
-# all_countrys_cases()
+all_countrys_cases()
