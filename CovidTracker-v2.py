6f28517805218084361d87853923015eb2585e31
v1s1t0r999 <aditya.funs.11@gmail.com>
2021-12-29 18:12:46 +0530

    Update CovidTracker-v2.py

--- CovidTracker-v2.py
+++ CovidTracker-v2.py
@@ -2,57 +2,74 @@ import requests as r
 
 
 class CountryNotFound(Exception):
-	pass
+    pass
 
 class CovidClient:
-	def __init__(self):
-		self.country=""
-		self.total_cases=""
-		self.total_death=""
-		self.total_recover=""
-		self.total_tests=""
-		self.active=""
-		self.critical=""
-		self.today_cases=""
-		self.today_deaths=""
-		self.today_recover=""
-		self.percent_cases=""
-		self.percent_death=""
-		self.percent_test==""
-		self.population==""
-		self.flag_url==""
-		self.country_id==""
+    def __init__(self):
+        self.country=""
+        self.total_cases=""
+        self.total_death=""
+        self.total_recover=""
+        self.total_tests=""
+        self.active=""
+        self.critical=""
+        self.today_cases=""
+        self.today_deaths=""
+        self.today_recover=""
+        self.percent_cases=""
+        self.percent_death=""
+        self.percent_test=""
+        self.population=""
+        self.flag_url=""
+        self.country_id=""
+        
+        
+    def _data(self):
+        """Internal Function to load data"""
+        s = r.get(self._get_base())
+        try:
+            s.json()['message']
+            return False
+        except KeyError:
+            return s.json()
+	
+	def _get_base(self):
+		"""Just to format the BASE url"""
+		return f"https://corona.lmao.ninja/v3/covid-19/countries/{self.country}"
+
+        
+    def load_country(self,country:str):
+		"""
+		Load a country to get its data.
+		Usage:
+		```
+		covid = CovidClient()
+		covid.load_country("Poland")
 		
 		
-	def _data(self):
-		"""Internal Function to load data"""
-		s = r.get(f"https://corona.lmao.ninja/v3/covid-19/countries/{self.country}")
-		try:
-			s.json()['message']
-			return False
-		except KeyError:
-			return s.json()
-		
-	def load_country(self,country:str):
-		self.country=country
-		d = self._data()
-		if self._data() is False:
-			raise CountryNotFound("No such Country Found!!")
-			return False
-		else:
-			self.total_cases=d['cases']
-			self.total_death=d['deaths']
-			self.total_recover=d['recovered']
-			self.total_tests=d['tests']
-			self.active = d['active']
-			self.critical = d['critical']
-			self.today_deaths = d['todayDeaths']
-			self.today_cases = d['todayCases']
-			self.today_recover=d['todayRecovered']
-			self.percent_cases=int(d['oneCasePerPeople'])/int(d['population'])*100
-			self.percent_death=int(d['oneDeathPerPeople'])/int(d['population'])*100
-			self.percent_test=int(d['oneTestPerPeople'])/int(d['population'])*100
-			self.population=d['population']
-			self.flag_url=d['countryInfo']['flag']
-			self.country_id=d['countryInfo']['_id']
-			return True
+		# Do your expensive stuff
+		print("Cases in Poland: {0}".format(covid.cases))
+		```
+		"""
+        self.country=country
+        d = self._data()
+        if self._data() is False:
+            raise CountryNotFound("No such Country Found!!")
+            return False
+        else:
+            self.total_cases=d['cases']
+            self.total_death=d['deaths']
+            self.total_recover=d['recovered']
+            self.total_tests=d['tests']
+            self.active = d['active']
+            self.critical = d['critical']
+            self.today_deaths = d['todayDeaths']
+            self.today_cases = d['todayCases']
+            self.today_recover=d['todayRecovered']
+            self.percent_cases=int(d['oneCasePerPeople'])/int(d['population'])*100
+            self.percent_death=int(d['oneDeathPerPeople'])/int(d['population'])*100
+            self.percent_test=int(d['oneTestPerPeople'])/int(d['population'])*100
+            self.population=d['population']
+            self.flag_url=d['countryInfo']['flag']
+            self.country_id=d['countryInfo']['_id']
+            return True
